fddb819a885975479c0595d48772fa6b0720e9b1
KTDVIP2k3 <Khanhtranduy996@gmail.com>
2025-10-21 21:13:07 +0700

    Demo soring security,http security to configure filter into security filter chain and authenticate manager

---
+++ Spring_Security/demo/demo/src/main/java/com/example/demo/Configure/SecurityConfigure.java
@@ -0,0 +1,80 @@
+package com.example.demo.Configure;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpCookie;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.HttpBasicDsl;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigure {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))    // Disable CSRF cho API
+                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/login").permitAll() // chỉ match path
+                        .anyRequest().authenticated()
+                )// tắt formLogin
+                .httpBasic(Customizer.withDefaults()); // nếu dùng Basic Auth
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(
+            UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return provider;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("john")
+                        .password(encoder.encode("password"))
+                        .roles("USER")
+                        .build(),
+                User.withUsername("admin")
+                        .password(encoder.encode("admin123"))
+                        .roles("ADMIN")
+                        .build()
+        );
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(); // Strong hashing
+    }
+
+}
--- Spring_Security/demo/demo/src/main/java/com/example/demo/Controller/HelloController.java
+++ Spring_Security/demo/demo/src/main/java/com/example/demo/Controller/HelloController.java
@@ -1,12 +1,25 @@
 package com.example.demo.Controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
 
     @GetMapping("/hello")
     public String hello(){
@@ -16,5 +29,21 @@ public class HelloController {
     @GetMapping("/goodbye")
     public String goodBye(){
         return "Good bye";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginDTO loginDTO){
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginDTO.getUserName(),
+                        loginDTO.getPassword() // gửi raw password, Spring sẽ tự check với PasswordEncoder
+                )
+        );
+        if(authentication.isAuthenticated()){
+            return  "Login succesfully";
+        }
+
+        return "Login fail";
     }
 }
